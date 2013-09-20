13f2fa5ad4912d207208b4f8cd2266080c567253
Erik <erik@Glados-studio-1555.(none)>
2013-09-20 19:05:47 -0400

    Fixed show name issue

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
   before_filter :authenticate_user!, except: [:index]
-  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  #before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
   # GET /pins
   # GET /pins.json
